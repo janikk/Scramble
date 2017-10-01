ae068766aacfa374952324a5ca9c53fd90a105b7
msik314 <msik314@gmail.com>
2017-10-01 11:42:23 -0400

    Fix Player collision

--- Scramble/Assets/MapActivator.cs
+++ Scramble/Assets/MapActivator.cs
@@ -6,11 +6,13 @@ public class MapActivator : MonoBehaviour {
     
     [SerializeField] private GameObject destructed;
     [SerializeField] private GameObject instantiated;
+    [SerializeField] private GameObject activated;
     
 	void OnTriggerEnter2D(Collider2D col)
     {
         if(col.gameObject.tag == "player")
         {
+            activated.SetActive(true);
             Transform t = destructed.transform;
             Destroy(destructed);
             Instantiate(instantiated, t.position, t.rotation);
