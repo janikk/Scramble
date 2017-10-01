55f5864ccb436280e15ae54960875a07ea3b946e
Kalvin Janik <janikk@rpi.edu>
2017-10-01 11:59:58 -0400

    level design furthered plus character stuff fixed

--- Scramble/Assets/PlayerController.cs
+++ Scramble/Assets/PlayerController.cs
@@ -22,18 +22,19 @@ public class PlayerController : MonoBehaviour {
 	void Update () {
 		lr = (int)Input.GetAxisRaw (axisLR);
 		ud = (int)Input.GetAxisRaw (axisUD);
-		if (Input.GetKeyDown ("m") && move) {
-			move = false;
-		}
-		if (Input.GetKeyDown ("m") && !move) {
+		float boxSize = 0.5f;
+		if (lr != 0 || ud != 0) {
 			move = true;
 		}
+		if (lr != 0 && ud != 0) {
+			boxSize *= Mathf.Sqrt (2.0f);
+		}
 		if (move) {
-            Vector3 test = new Vector3(movespeed * lr * Time.deltaTime * Time.deltaTime, movespeed * ud * Time.deltaTime * Time.deltaTime, 0);
-            RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, test.normalized, test.magnitude, layerMask);
+			Vector3 test = new Vector3(movespeed * lr * Time.deltaTime * Time.deltaTime, movespeed * ud * Time.deltaTime * Time.deltaTime, 0);
+            RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position + test.normalized * boxSize, test.normalized, test.magnitude, layerMask);
             foreach(RaycastHit2D r in hits)
             {
-                if(r.collider.gameObject.tag == "wall");
+                if(r.collider.gameObject.tag == "wall")
                 {
                     move = false;
                     break;
