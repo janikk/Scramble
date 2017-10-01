c8e181addc05cdef2da5279796f095b43068ce82
msik314 <msik314@gmail.com>
2017-10-01 10:32:47 -0400

    Add MapActivate script

---
+++ Scramble/Assets/MapActivator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapActivator : MonoBehaviour {
+    
+    [SerializeField] private GameObject destructed;
+    [SerializeField] private GameObject instantiated;
+    
+	void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "player")
+        {
+            Transform t = destructed.transform;
+            Destroy(destructed);
+            Instantiate(instantiated, t.position, t.rotation);
+            Destroy(gameObject);
+        }
+    }
+}
