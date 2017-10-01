784219306120918807e6f21231870f68b2158f89
msik314 <msik314@gmail.com>
2017-10-01 00:35:25 -0400

    Add Camera/Sublevel script

---
+++ Scramble/Assets/SubLevel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+
+public class SubLevel : MonoBehaviour {
+	// Use this for initialization
+	void Start()
+    {
+		
+	}
+	
+	void Awake()
+    {
+        GetComponent<Collider2D>().isTrigger = true;
+    }
+	
+	// Update is called once per frame
+	void Update()
+    {
+		
+	}
+	
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.gameObject.tag == "enemy")
+        {
+            Destroy(col.gameObject);
+        }
+        else if(col.gameObject.tag == "player")
+        {
+            Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, Camera.main.transform.position.z);
+        }
+    }
+}
